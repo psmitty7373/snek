8f16607d383074bf0042b6ea466b8becbb6d6f8b
psmitty7373 <psmitty7373@gmail.com>
2018-09-08 22:11:19 +0000

    - Final alpha commits for the client

--- test_server.py
+++ test_server.py
@@ -7,8 +7,8 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('localhost', 55553))
 sock.listen(5)
 def get_fake_sneks():
-    sneks = '\x16'
-    for i in range(1,17):
+    sneks = '\x10'
+    for i in range(0,16):
         sneks += chr(i) + '\x00\x00\x00\x00'
     return sneks
 
@@ -23,11 +23,11 @@ def get_fake_board():
 
 while True:
     conn, addr = sock.accept()
-    conn.send('\x01\x01')
+    conn.send('\x00\x03')
     while True:
         try:
             msg = get_fake_sneks() + get_fake_board()
-            msg = struct.pack('!i', len(msg)) + msg
+            msg = '\x02' + struct.pack('!i', len(msg)) + msg
             conn.send(msg)
             time.sleep(0.25)
         except Exception as e:
